4931edcab48b54ee97a75146642c45a3f90c6828
DimaRyabokon <d.ryabokon@gmail.com>
2018-11-08 14:57:22 +0200

    images folder

--- demo_part_01.py
+++ demo_part_01.py
@@ -1,53 +1,62 @@
 import cv2
 import numpy
-import tools_draw as Draw
-import tools_calibrate as Calibrate
+import os
+import tools_calibrate
+import tools_IO
 import math
 #--------------------------------------------------------------------------------------------------------------------------
 def example_01_calibrate_camera():
 
-    folder_input_chess      = 'D:/Projects/Cyb/Stereo/ex01/left/'
-    filename_input_chess    = 'D:/Projects/Cyb/Stereo/ex01/left/left01.jpg'
-    filename_input_grid     = 'D:/Projects/Cyb/Stereo/ex01/grid.jpg'
-    filename_output_chess   = 'D:/Projects/Cyb/Stereo/ex01_out/undistorted_chess.jpg'
-    filename_output_grid    = 'D:/Projects/Cyb/Stereo/ex01_out/undistorted_grid.jpg'
+    filename_input_grid     = '_images/ex01/grid.jpg'
+    folder_input_chess      = '_images/ex01/left/'
+    filename_input_chess    = folder_input_chess + 'left01.jpg'
 
+    folder_output           = '_images/ex01_out/'
+    filename_output_chess   = folder_output + 'undistorted_chess.jpg'
+    filename_output_grid    = folder_output + 'undistorted_grid.jpg'
     chess_rows=6
     chess_cols=7
 
-    cameraMatrix, dist = Calibrate.get_proj_dist_mat_for_images(folder_input_chess,chess_rows,chess_cols)
-    undistorted_chess  = Calibrate.undistort_image(cameraMatrix, dist, filename_input_chess)
-    undistorted_grid   = Calibrate.undistort_image(cameraMatrix, dist, filename_input_grid)
+    if not os.path.exists(folder_output):
+        os.makedirs(folder_output)
+    else:
+        tools_IO.remove_files(folder_output)
+
+    cameraMatrix, dist = tools_calibrate.get_proj_dist_mat_for_images(folder_input_chess,chess_rows,chess_cols)
+    undistorted_chess  = tools_calibrate.undistort_image(cameraMatrix, dist, filename_input_chess)
+    undistorted_grid   = tools_calibrate.undistort_image(cameraMatrix, dist, filename_input_grid)
     cv2.imwrite(filename_output_chess, undistorted_chess)
     cv2.imwrite(filename_output_grid , undistorted_grid)
 
-    #print('cameraMatrix')
-    #print(cameraMatrix)
-    #print('dist')
-    #print(dist)
     return
 # -------------------------------------------------------------------------------------------------------------------------
 def example_02_rectify_pair():
 
-    folder_input      = 'D:/Projects/Cyb/Stereo/ex01/left/'
-    filename_input_1  = 'D:/Projects/Cyb/Stereo/ex01/left/left01.jpg'
-    filename_input_2  = 'D:/Projects/Cyb/Stereo/ex01/right/right01.jpg'
-    filename_output_1 = 'D:/Projects/Cyb/Stereo/ex01_out/left01_rect.jpg'
-    filename_output_2 = 'D:/Projects/Cyb/Stereo/ex01_out/right01_rect.jpg'
+    folder_input_chess = '_images/ex01/left/'
+    folder_input       = '_images/ex01/'
+    filename_input_1   = folder_input + 'left/left01.jpg'
+    filename_input_2   = folder_input + 'right/right01.jpg'
+    folder_output      = '_images/ex01_out/'
+    filename_output_1  = folder_output + 'left01_rect.jpg'
+    filename_output_2  = folder_output + 'right01_rect.jpg'
 
     chess_rows=6
     chess_cols=7
 
-    cameraMatrix, dist = Calibrate.get_proj_dist_mat_for_images(folder_input,chess_rows,chess_cols)
+    if not os.path.exists(folder_output):
+        os.makedirs(folder_output)
+    else:
+        tools_IO.remove_files(folder_output)
 
-    im1_remapped, im2_remapped = Calibrate.rectify_pair(cameraMatrix, dist, filename_input_1, filename_input_2, chess_rows, chess_cols)
+        cameraMatrix, dist = tools_calibrate.get_proj_dist_mat_for_images(folder_input_chess, chess_rows, chess_cols)
+
+    im1_remapped, im2_remapped = tools_calibrate.rectify_pair(cameraMatrix, dist, filename_input_1, filename_input_2, chess_rows, chess_cols)
     cv2.imwrite(filename_output_1, im1_remapped)
     cv2.imwrite(filename_output_2, im2_remapped)
 
     return
 
 #==========================================================================================================================
-
 def example_03_find_homography_manual():
 
     im_scene                = cv2.imread('D:/Projects/Cyb/Stereo/ex03_manual/scene.jpg')
@@ -109,47 +118,47 @@ def example_03_find_homography_SIFT():
 #--------------------------------------------------------------------------------------------------------------------------
 def example_03_find_homography_ECC():
 
-	#im1 = cv2.imread(       'D:/Projects/Cyb/Stereo/ex03_homo_affine/image01.bmp')
-	#im2 = cv2.imread(       'D:/Projects/Cyb/Stereo/ex03_homo_affine/image02.bmp')
+    #im1 = cv2.imread(       'D:/Projects/Cyb/Stereo/ex03_homo_affine/image01.bmp')
+    #im2 = cv2.imread(       'D:/Projects/Cyb/Stereo/ex03_homo_affine/image02.bmp')
 
-	im1 = cv2.imread(       'D:/Projects/Cyb/Stereo/ex03_homo_affine/000.bmp')
-	im2 = cv2.imread(       'D:/Projects/Cyb/Stereo/ex03_homo_affine/007.bmp')
-	output_filename_A1_T =  'D:/Projects/Cyb/Stereo/ex03_homo_affine/image01_aligned_transf.bmp'
-	output_filename_A2_T =  'D:/Projects/Cyb/Stereo/ex03_homo_affine/image02_aligned_transf.bmp'
-	output_filename_A1_H =  'D:/Projects/Cyb/Stereo/ex03_homo_affine/image01_aligned_homogr.bmp'
-	output_filename_A2_H =  'D:/Projects/Cyb/Stereo/ex03_homo_affine/image02_aligned_homogr.bmp'
+    im1 = cv2.imread(       'D:/Projects/Cyb/Stereo/ex03_homo_affine/000.bmp')
+    im2 = cv2.imread(       'D:/Projects/Cyb/Stereo/ex03_homo_affine/007.bmp')
+    output_filename_A1_T =  'D:/Projects/Cyb/Stereo/ex03_homo_affine/image01_aligned_transf.bmp'
+    output_filename_A2_T =  'D:/Projects/Cyb/Stereo/ex03_homo_affine/image02_aligned_transf.bmp'
+    output_filename_A1_H =  'D:/Projects/Cyb/Stereo/ex03_homo_affine/image01_aligned_homogr.bmp'
+    output_filename_A2_H =  'D:/Projects/Cyb/Stereo/ex03_homo_affine/image02_aligned_homogr.bmp'
 
-	im1_gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-	im2_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-	sz = im1.shape
+    im1_gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+    sz = im1.shape
 
-	number_of_iterations = 5000
-	termination_eps = 1e-10
-	criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations, termination_eps)
+    number_of_iterations = 5000
+    termination_eps = 1e-10
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations, termination_eps)
 
-	warp_mode = cv2.MOTION_TRANSLATION
-	(cc, warp_matrix12) = cv2.findTransformECC(im1_gray, im2_gray, numpy.eye(2, 3, dtype=numpy.float32), warp_mode,criteria)
-	(cc, warp_matrix21) = cv2.findTransformECC(im2_gray, im1_gray, numpy.eye(2, 3, dtype=numpy.float32), warp_mode,criteria)
-	im2_aligned = cv2.warpAffine(im2, warp_matrix12, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
-	im1_aligned = cv2.warpAffine(im1, warp_matrix21, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
-	cv2.imwrite(output_filename_A1_T, im1_aligned)
-	cv2.imwrite(output_filename_A2_T, im2_aligned)
+    warp_mode = cv2.MOTION_TRANSLATION
+    (cc, warp_matrix12) = cv2.findTransformECC(im1_gray, im2_gray, numpy.eye(2, 3, dtype=numpy.float32), warp_mode,criteria)
+    (cc, warp_matrix21) = cv2.findTransformECC(im2_gray, im1_gray, numpy.eye(2, 3, dtype=numpy.float32), warp_mode,criteria)
+    im2_aligned = cv2.warpAffine(im2, warp_matrix12, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    im1_aligned = cv2.warpAffine(im1, warp_matrix21, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    cv2.imwrite(output_filename_A1_T, im1_aligned)
+    cv2.imwrite(output_filename_A2_T, im2_aligned)
 
-	'''
-	warp_mode = cv2.MOTION_HOMOGRAPHY
-	(cc, warp_matrix12) = cv2.findTransformECC(im1_gray, im2_gray, numpy.eye(3, 3, dtype=numpy.float32), warp_mode, (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5000, 1e-10))
-	(cc, warp_matrix21) = cv2.findTransformECC(im2_gray, im1_gray, numpy.eye(3, 3, dtype=numpy.float32), warp_mode, (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5000, 1e-10))
-	im2_aligned = cv2.warpPerspective(im2, warp_matrix12, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
-	im1_aligned = cv2.warpPerspective(im1, warp_matrix21, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
-	cv2.imwrite(output_filename_A1_H, im1_aligned)
-	cv2.imwrite(output_filename_A2_H, im2_aligned)
-	'''
+    '''
+    warp_mode = cv2.MOTION_HOMOGRAPHY
+    (cc, warp_matrix12) = cv2.findTransformECC(im1_gray, im2_gray, numpy.eye(3, 3, dtype=numpy.float32), warp_mode, (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5000, 1e-10))
+    (cc, warp_matrix21) = cv2.findTransformECC(im2_gray, im1_gray, numpy.eye(3, 3, dtype=numpy.float32), warp_mode, (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5000, 1e-10))
+    im2_aligned = cv2.warpPerspective(im2, warp_matrix12, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    im1_aligned = cv2.warpPerspective(im1, warp_matrix21, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    cv2.imwrite(output_filename_A1_H, im1_aligned)
+    cv2.imwrite(output_filename_A2_H, im2_aligned)
+    '''
 
-	return
+    return
 #--------------------------------------------------------------------------------------------------------------------------
 def example_03_find_homography_multiple():
 
-	return
+    return
 # --------------------------------------------------------------------------------------------------------------------------
 def example_03_find_homography_live():
 
@@ -187,98 +196,98 @@ def example_03_find_homography_live():
 #==========================================================================================================================
 
 def example_04_blend_avg():
-	img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneA_left.png')
-	img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneA_rght.png')
-	cv2.imwrite(      'D:/Projects/Cyb/Stereo/ex04_blend/sceneA_mix_avg.png', Calibrate.blend_avg(img1, img2,(0,0,0)))
+    img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneA_left.png')
+    img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneA_rght.png')
+    cv2.imwrite(      'D:/Projects/Cyb/Stereo/ex04_blend/sceneA_mix_avg.png', Calibrate.blend_avg(img1, img2,(0,0,0)))
 
-	img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneB_left.png')
-	img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneB_rght.png')
-	cv2.imwrite(      'D:/Projects/Cyb/Stereo/ex04_blend/sceneB_mix_avg.png', Calibrate.blend_avg(img1, img2,(255,255,255)))
-	return
+    img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneB_left.png')
+    img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneB_rght.png')
+    cv2.imwrite(      'D:/Projects/Cyb/Stereo/ex04_blend/sceneB_mix_avg.png', Calibrate.blend_avg(img1, img2,(255,255,255)))
+    return
 #--------------------------------------------------------------------------------------------------------------------------
 def example_04_blend_multi_band():
-	img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneA_left.png')
-	img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneA_rght.png')
-	cv2.imwrite(      'D:/Projects/Cyb/Stereo/ex04_blend/sceneA_mix_multi.png', Calibrate.blend_multi_band(img1, img2,(0,0,0)))
+    img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneA_left.png')
+    img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneA_rght.png')
+    cv2.imwrite(      'D:/Projects/Cyb/Stereo/ex04_blend/sceneA_mix_multi.png', Calibrate.blend_multi_band(img1, img2,(0,0,0)))
 
-	img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneB_left.png')
-	img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneB_rght.png')
-	cv2.imwrite(      'D:/Projects/Cyb/Stereo/ex04_blend/sceneB_mix_multi.png', Calibrate.blend_multi_band(img1, img2,(255,255,255)))
-	return
+    img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneB_left.png')
+    img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex04_blend/sceneB_rght.png')
+    cv2.imwrite(      'D:/Projects/Cyb/Stereo/ex04_blend/sceneB_mix_multi.png', Calibrate.blend_multi_band(img1, img2,(255,255,255)))
+    return
 #--------------------------------------------------------------------------------------------------------------------------
 def example_04_find_homography_blend_multi_band():
 
-	img_left  = cv2.imread('D:/Projects/Cyb/Stereo/ex03_homography_blend_multi_band/left.jpg')
-	img_right = cv2.imread('D:/Projects/Cyb/Stereo/ex03_homography_blend_multi_band/rght.jpg')
-	output_left          = 'D:/Projects/Cyb/Stereo/ex03_homography_blend_multi_band/left_res.jpg'
-	output_right         = 'D:/Projects/Cyb/Stereo/ex03_homography_blend_multi_band/rght_res.jpg'
+    img_left  = cv2.imread('D:/Projects/Cyb/Stereo/ex03_homography_blend_multi_band/left.jpg')
+    img_right = cv2.imread('D:/Projects/Cyb/Stereo/ex03_homography_blend_multi_band/rght.jpg')
+    output_left          = 'D:/Projects/Cyb/Stereo/ex03_homography_blend_multi_band/left_res.jpg'
+    output_right         = 'D:/Projects/Cyb/Stereo/ex03_homography_blend_multi_band/rght_res.jpg'
 
-	homography = Calibrate.get_sift_homography(img_right, img_left)
-	result_right, result_left = Calibrate.get_stitched_images(img_left, img_right, homography)
-	result_image = Calibrate.blend_multi_band(result_left, result_right)
-	cv2.imwrite(output_left, result_image)
+    homography = Calibrate.get_sift_homography(img_right, img_left)
+    result_right, result_left = Calibrate.get_stitched_images(img_left, img_right, homography)
+    result_image = Calibrate.blend_multi_band(result_left, result_right)
+    cv2.imwrite(output_left, result_image)
 
-	homography = Calibrate.get_sift_homography(img_left, img_right)
-	result_left, result_right  = Calibrate.get_stitched_images(img_right, img_left, homography)
-	result_image = Calibrate.blend_multi_band(result_left, result_right)
-	cv2.imwrite(output_right, result_image)
-	return
+    homography = Calibrate.get_sift_homography(img_left, img_right)
+    result_left, result_right  = Calibrate.get_stitched_images(img_right, img_left, homography)
+    result_image = Calibrate.blend_multi_band(result_left, result_right)
+    cv2.imwrite(output_right, result_image)
+    return
 
 #==========================================================================================================================
 def example_05_derive_transform():
-	img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex05_derive_transform/base.png')
-	img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex05_derive_transform/deform.png')
-	output_filename1 = 'D:/Projects/Cyb/Stereo/ex05_derive_transform/res1.png'
-	output_filename2 = 'D:/Projects/Cyb/Stereo/ex05_derive_transform/res2.png'
-	R, T, normal, HH, K = Calibrate.derive_transform(img1,img2)
-	return R, T, normal, HH, K
+    img1 = cv2.imread('D:/Projects/Cyb/Stereo/ex05_derive_transform/base.png')
+    img2 = cv2.imread('D:/Projects/Cyb/Stereo/ex05_derive_transform/deform.png')
+    output_filename1 = 'D:/Projects/Cyb/Stereo/ex05_derive_transform/res1.png'
+    output_filename2 = 'D:/Projects/Cyb/Stereo/ex05_derive_transform/res2.png'
+    R, T, normal, HH, K = Calibrate.derive_transform(img1,img2)
+    return R, T, normal, HH, K
 # --------------------------------------------------------------------------------------------------------------------------
 def example_05_animage_homography():
-	img_left  = cv2.imread('D:/Projects/Cyb/Stereo/ex05_homo_middle/left.jpg')
-	img_rght  = cv2.imread('D:/Projects/Cyb/Stereo/ex05_homo_middle/rght.jpg')
-	output_filename     = 'D:/Projects/Cyb/Stereo/ex05_homo_middle/res'
-	R, T, normal, H, K = Calibrate.derive_transform(img_left, img_rght)
+    img_left  = cv2.imread('D:/Projects/Cyb/Stereo/ex05_homo_middle/left.jpg')
+    img_rght  = cv2.imread('D:/Projects/Cyb/Stereo/ex05_homo_middle/rght.jpg')
+    output_filename     = 'D:/Projects/Cyb/Stereo/ex05_homo_middle/res'
+    R, T, normal, H, K = Calibrate.derive_transform(img_left, img_rght)
 
-	for alpha in range(0,20):
-		HH = Calibrate.bend_homography(H, alpha/20.0)
-		result_right, result_left = Calibrate.get_stitched_images(img_rght, img_left, HH)
-		cv2.imwrite((output_filename+('%02d.png' % alpha)),result_right )
+    for alpha in range(0,20):
+        HH = Calibrate.bend_homography(H, alpha/20.0)
+        result_right, result_left = Calibrate.get_stitched_images(img_rght, img_left, HH)
+        cv2.imwrite((output_filename+('%02d.png' % alpha)),result_right )
 
-	return
+    return
 # --------------------------------------------------------------------------------------------------------------------------
 def example_05_find_homography_bend():
 
-	img_left  = cv2.imread('D:/Projects/Cyb/Stereo/ex05_homo_middle/left.jpg')
-	img_rght  = cv2.imread('D:/Projects/Cyb/Stereo/ex05_homo_middle/rght.jpg')
-	output    =            'D:/Projects/Cyb/Stereo/ex05_homo_middle/res.jpg'
-	output1   =            'D:/Projects/Cyb/Stereo/ex05_homo_middle/res1.jpg'
-	output2   =            'D:/Projects/Cyb/Stereo/ex05_homo_middle/res2.jpg'
+    img_left  = cv2.imread('D:/Projects/Cyb/Stereo/ex05_homo_middle/left.jpg')
+    img_rght  = cv2.imread('D:/Projects/Cyb/Stereo/ex05_homo_middle/rght.jpg')
+    output    =            'D:/Projects/Cyb/Stereo/ex05_homo_middle/res.jpg'
+    output1   =            'D:/Projects/Cyb/Stereo/ex05_homo_middle/res1.jpg'
+    output2   =            'D:/Projects/Cyb/Stereo/ex05_homo_middle/res2.jpg'
 
 
-	#R, T, normal, H, K = Calibrate.derive_transform(img_left, img_rght)
-	#left_coord  = numpy.float32([[377,237],[469,237],[471,470],[377,374]]).reshape(-1, 1, 2)
-	#right_coord = numpy.float32([[86, 177],[179,178],[179,269],[80, 267]]).reshape(-1, 1, 2)
+    #R, T, normal, H, K = Calibrate.derive_transform(img_left, img_rght)
+    #left_coord  = numpy.float32([[377,237],[469,237],[471,470],[377,374]]).reshape(-1, 1, 2)
+    #right_coord = numpy.float32([[86, 177],[179,178],[179,269],[80, 267]]).reshape(-1, 1, 2)
 
-	#B  = Calibrate.bend_homography(H, 0.5)
-	#B2 = Calibrate.get_inverse_homography(Calibrate.bend_homography(H, 0.5))
-	#print(numpy.dot(B,B2))
-	#print(numpy.dot(B2,B))
-	#print()
+    #B  = Calibrate.bend_homography(H, 0.5)
+    #B2 = Calibrate.get_inverse_homography(Calibrate.bend_homography(H, 0.5))
+    #print(numpy.dot(B,B2))
+    #print(numpy.dot(B2,B))
+    #print()
 
-	#left_coord_transf = cv2.perspectiveTransform(left_coord, B)
-	#print(left_coord_transf.reshape(4, 2))
-	#right_coord_transf = cv2.perspectiveTransform(right_coord, B2)
-	#print(right_coord_transf.reshape(4,2))
+    #left_coord_transf = cv2.perspectiveTransform(left_coord, B)
+    #print(left_coord_transf.reshape(4, 2))
+    #right_coord_transf = cv2.perspectiveTransform(right_coord, B2)
+    #print(right_coord_transf.reshape(4,2))
 
 
-	H_left,H_right = Calibrate.get_homography_middle(img_left, img_rght)
-	result_left,result_right = Calibrate.get_stitched_images_middle(img_left,img_rght, H_left,H_right)
-	result_image = Calibrate.blend_multi_band(result_left, result_right)
-	cv2.imwrite(output1, result_left)
-	cv2.imwrite(output2, result_right)
-	cv2.imwrite(output, result_image)
+    H_left,H_right = Calibrate.get_homography_middle(img_left, img_rght)
+    result_left,result_right = Calibrate.get_stitched_images_middle(img_left,img_rght, H_left,H_right)
+    result_image = Calibrate.blend_multi_band(result_left, result_right)
+    cv2.imwrite(output1, result_left)
+    cv2.imwrite(output2, result_right)
+    cv2.imwrite(output, result_image)
 
-	return
+    return
 #==========================================================================================================================
 def example_XX_pose_estimation(foldername,cameraMatrix, dist):
 
@@ -313,7 +322,7 @@ if __name__ == '__main__':
 
     numpy.set_printoptions(suppress=True, precision=0)
     #example_01_calibrate_camera()
-    #example_02_rectify_pair()
+    example_02_rectify_pair()
 
     #example_03_find_homography_manual()
     #example_03_find_homography_BFMatcher()
