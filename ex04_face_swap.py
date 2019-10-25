a7ede40f75ee27d9d3557fa3ffef8b1470f36286
Dmytro Ryabokon <dmytro.ryabokon@synapse.com>
2019-10-25 10:46:08 +0300

    faceswap

--- ex04_face_swap.py
+++ ex04_face_swap.py
@@ -37,58 +37,76 @@ def main():
     return
 # ---------------------------------------------------------------------------------------------------------------------
 def demo_live(filename_out):
-    image1 = cv2.imread('./images/ex_faceswap/01/personC-1.jpg')
-    image2 = cv2.imread('./images/ex_faceswap/01/personD-2.jpg')
+
+    use_camera = False
+    prefix = './images/ex_faceswap/01/'
+    filename1='personC-1.jpg'
+    filename2='personB-4.jpg'
+
+    image1 = cv2.imread(prefix+filename1)
+    image2 = cv2.imread(prefix+filename2)
     L1_original = D.get_landmarks(image1)
     del_triangles = Delaunay(L1_original).vertices
+    L2_original = D.get_landmarks(image2)
 
+    result = tools_landmark.do_transfer(image1, image2, L1_original, L2_original, del_triangles)
 
     cap = cv2.VideoCapture(0)
     cnt, start_time, fps = 0, time.time(), 0
     while (True):
-        ret, image2 = cap.read()
-
-        L2_original = D.get_landmarks(image2)
-        result2 = tools_landmark.do_transfer(image1, image2, L1_original, L2_original, del_triangles)
-        #result2 = D.draw_landmarks(image2)
+        if use_camera:
+            ret, image2 = cap.read()
+            L2_original = D.get_landmarks(image2)
+            result = tools_landmark.do_transfer(image1, image2, L1_original, L2_original, del_triangles)
 
         if time.time() > start_time: fps = cnt / (time.time() - start_time)
-        result2 = cv2.putText(result2, '{0: 1.1f} {1}'.format(fps, ' fps'), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(0, 0, 0), 1, cv2.LINE_AA)
+        result2 = result.copy()
+        result2 = cv2.putText(result2, '{0: 1.1f} {1}'.format(fps, ' fps'), (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 0), 1, cv2.LINE_AA)
+        result2 = cv2.putText(result2, '{0}'.format(filename1), (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 0), 1, cv2.LINE_AA)
+        result2 = cv2.putText(result2, '{0}'.format(filename2), (0, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 0), 1,cv2.LINE_AA)
         cv2.imshow('frame', result2)
         cnt += 1
         key = cv2.waitKey(1)
+
         if key & 0xFF == 27:break
-
-        if key & 0xFF == ord('1'): image1 = cv2.imread('./images/ex_faceswap/01/personA-2.jpg')
-        if key & 0xFF == ord('2'): image1 = cv2.imread('./images/ex_faceswap/01/personB-4.jpg')
-        if key & 0xFF == ord('3'): image1 = cv2.imread('./images/ex_faceswap/01/personC-1.jpg')
-        if key & 0xFF == ord('4'): image1 = cv2.imread('./images/ex_faceswap/01/personC-2.jpg')
-        if key & 0xFF == ord('5'): image1 = cv2.imread('./images/ex_faceswap/01/personD-1.jpg')
-        if key & 0xFF == ord('6'): image1 = cv2.imread('./images/ex_faceswap/01/personE-1.jpg')
-        if key & 0xFF == ord('7'): image1 = cv2.imread('./images/ex_faceswap/01/personH-2.jpg')
-        if key & 0xFF == ord('8'): image1 = cv2.imread('./images/ex_faceswap/01/personL-1.jpg')
-
-        if key & 0xFF == ord('a'): image2 = cv2.imread('./images/ex_faceswap/01/personA-2.jpg')
-        if key & 0xFF == ord('b'): image2 = cv2.imread('./images/ex_faceswap/01/personB-4.jpg')
-        if key & 0xFF == ord('c'): image2 = cv2.imread('./images/ex_faceswap/01/personC-1.jpg')
-        if key & 0xFF == ord('d'): image2 = cv2.imread('./images/ex_faceswap/01/personD-2.jpg')
-        if key & 0xFF == ord('e'): image2 = cv2.imread('./images/ex_faceswap/01/personE-1.jpg')
-        if key & 0xFF == ord('f'): image2 = cv2.imread('./images/ex_faceswap/01/personF-1.jpg')
-        if key & 0xFF == ord('h'): image2 = cv2.imread('./images/ex_faceswap/01/personH-1.jpg')
-        if key & 0xFF == ord('j'): image2 = cv2.imread('./images/ex_faceswap/01/personJ-1.jpg')
-        if key & 0xFF == ord('k'): image2 = cv2.imread('./images/ex_faceswap/01/personK-1.jpg')
-        if key & 0xFF == ord('l'): image2 = cv2.imread('./images/ex_faceswap/01/personL-1.jpg')
+        if key & 0xFF == ord('1'): filename1 = 'personA-2.jpg'
+        if key & 0xFF == ord('2'): filename1 = 'personB-4.jpg'
+        if key & 0xFF == ord('3'): filename1 = 'personC-1.jpg'
+        if key & 0xFF == ord('4'): filename1 = 'personC-2.jpg'
+        if key & 0xFF == ord('5'): filename1 = 'personD-1.jpg'
+        if key & 0xFF == ord('6'): filename1 = 'personE-1.jpg'
+        if key & 0xFF == ord('7'): filename1 = 'personF-1.jpg'
+        if key & 0xFF == ord('8'): filename1 = 'personJ-1.jpg'
+        if key & 0xFF == ord('9'): filename1 = 'personK-1.jpg'
 
         if (key & 0xFF >= ord('1')) and (key & 0xFF <= ord('9')):
+            image1 = cv2.imread(prefix + filename1)
             L1_original = D.get_landmarks(image1)
             del_triangles = Delaunay(L1_original).vertices
+            result = tools_landmark.do_transfer(image1, image2, L1_original, L2_original, del_triangles)
+
+
+        if key & 0xFF == ord('q'): filename2 = 'personA-2.jpg'
+        if key & 0xFF == ord('w'): filename2 = 'personB-4.jpg'
+        if key & 0xFF == ord('e'): filename2 = 'personC-1.jpg'
+        if key & 0xFF == ord('r'): filename2 = 'personD-1.jpg'
+        if key & 0xFF == ord('t'): filename2 = 'personE-1.jpg'
+        if key & 0xFF == ord('y'): filename2 = 'personF-1.jpg'
+        if key & 0xFF == ord('u'): filename2 = 'personH-1.jpg'
+        if key & 0xFF == ord('i'): filename2 = 'personJ-1.jpg'
+        if key & 0xFF == ord('o'): filename2 = 'personK-1.jpg'
+        if key & 0xFF == ord('p'): filename2 = 'personL-1.jpg'
+        if key & 0xFF == ord('-'): use_camera = True;filename2='cam'
+        if (key & 0xFF >= ord('a')) and (key & 0xFF <= ord('x')):
+            image2 = cv2.imread(prefix + filename2)
+            L2_original = D.get_landmarks(image2)
+            use_camera = False
+            result = tools_landmark.do_transfer(image1, image2, L1_original, L2_original, del_triangles)
 
         if (key & 0xFF == 13) or (key & 0xFF == 32):
-            default_filename_in = '1.jpg'
-            default_filename_in2 = '2.jpg'
-            cv2.imwrite(default_filename_in , image1)
-            cv2.imwrite(default_filename_in2, image2)
-            tools_landmark.transferface_first_to_second(D, default_filename_in, default_filename_in2, default_folder_out)
+            cv2.imwrite('1.jpg', image1)
+            cv2.imwrite('2.jpg', image2)
+            tools_landmark.transferface_first_to_second(D, '1.jpg', '2.jpg', default_folder_out)
 
     cap.release()
     cv2.destroyAllWindows()
