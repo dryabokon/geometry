ad188eee22077839ce53c3b77475f6ecc6b97a1d
Dima <d.ryabokon@gmail.com>
2020-04-14 16:53:06 +0300

    lines

--- ex23_line_detection.py
+++ ex23_line_detection.py
@@ -4,6 +4,7 @@ import tools_soccer_field
 import tools_GL3D
 import tools_pr_geom
 import tools_IO
+import tools_Skeletone
 #----------------------------------------------------------------------------------------------------------------------
 folder_in = './images/ex_lines/'
 folder_out = './images/output/'
@@ -11,18 +12,13 @@ filename_in = './images/ex_lines/frame000159.jpg'
 #filename_in = './images/ex_lines/Image1.jpg'
 SFP = tools_soccer_field.Soccer_Field_Processor()
 # ----------------------------------------------------------------------------------------------------------------------
-def example_01(filename_in,do_debug=1):
-    #tools_IO.remove_files(folder_out)
-    image = cv2.imread(filename_in)
-    result = SFP.process_view(image, do_debug)
+def process_folder(folder_in, folder_out):
+    tools_IO.remove_files(folder_out, create=True)
+    filenames = tools_IO.get_filenames(folder_in, '*.jpg')
 
-    cv2.imwrite(folder_out + filename_in.split('/')[-1].split('.')[0]+'.png', result)
+    for filename in filenames[2:3]:
+        SFP.process_view(folder_in+filename,do_debug=True)
 
-    return
-# ----------------------------------------------------------------------------------------------------------------------
-def example_02(folder_in,folder_out):
-    tools_IO.remove_files(folder_out)
-    SFP.process_folder(folder_in, folder_out)
 
     return
 # ----------------------------------------------------------------------------------------------------------------------
@@ -34,8 +30,10 @@ def example_03():
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    example_01(filename_in)
-    #example_02(folder_in,folder_out)
+    process_folder(folder_in,folder_out)
+
+
+
 
 
 
--- ex24_binarization.py
+++ ex24_binarization.py
@@ -3,8 +3,6 @@ import numpy
 import tools_IO
 import processor_Slices
 import tools_Skeletone
-import tools_image
-import tools_plot
 #----------------------------------------------------------------------------------------------------------------------
 folder_in = './images/ex_bin/'
 folder_out = './images/output/'
@@ -12,61 +10,17 @@ folder_out = './images/output/'
 P = processor_Slices.processor_Slices(folder_out)
 S = tools_Skeletone.Skelenonizer()
 # ----------------------------------------------------------------------------------------------------------------------
-def process_avg(folder_in, folder_out):
-
-    filenames = tools_IO.get_filenames(folder_in, '*.png')
-
-    image = cv2.imread(folder_in + filenames[0])
-    A = numpy.zeros_like(image,numpy.long)
-
-    for filename in filenames:
-        image = cv2.imread(folder_in + filename)
-        A += image
-
-    A = A / len(filenames)
-    A/=A.max()
-    A*=255
-
-    cv2.imwrite(folder_out+'avg_dir.png',tools_image.hitmap2d_to_jet(A))
-
-    return
-# ----------------------------------------------------------------------------------------------------------------------
-def mask_images(filename1_in,filename2_in):
-    image1 = cv2.imread(filename1_in)
-    image2 = cv2.imread(filename2_in)
-    result = numpy.zeros_like(image2)
-
-    for r in range(image1.shape[0]):
-        for c in range(image1.shape[1]):
-            col = image1[r,c]
-            hsv = tools_image.bgr2hsv(col).reshape(3)
-            l = image2[r, c,0]
-
-            hsv[2]=l
-            col = tools_image.hsv2bgr(hsv)
-            result[r,c]=col
-
-
-    cv2.imwrite(folder_out+'res.png',result)
-
-    return
-# ----------------------------------------------------------------------------------------------------------------------
 def process_folder(folder_in, folder_out):
 
     tools_IO.remove_files(folder_out)
     filenames = tools_IO.get_filenames(folder_in,'*.jpg')
 
-    for filename in filenames[60:]:
-        #P.process_file_ske(folder_in + filename, do_debug=True)
-        #P.process_file_granules(folder_in + filename, do_debug=True)
-        P.process_file_flow(folder_in + filename, do_debug=True)
+    for filename in filenames:
+        P.process_file_granules(folder_in + filename, do_debug=True)
 
     return
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    #process_folder(folder_in, folder_out)
-    #process_avg(folder_out, folder_out)
-
-    mask_images(folder_out+'avg_dir2.png', folder_out+'avg2.png')
+    process_folder(folder_in, folder_out)
 
