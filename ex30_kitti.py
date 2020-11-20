9576013d2f281aa0feedb2d01a70ecb381969e2a
dryabokon <d.ryabokon@gmail.com>
2020-11-20 22:07:39 +0200

    BEV

--- ex30_kitti.py
+++ ex30_kitti.py
@@ -593,29 +593,6 @@ def points_to_lines(points):
 
     return lines
 # ----------------------------------------------------------------------------------------------------------------------
-def get_h_ipersp(image,target_W,target_H,point_van_xy = (631, 166),):
-    H, W = image.shape[:2]
-
-    tol_up = 20
-    tol_bottom = 0
-    upper_line = (0, point_van_xy[1] + tol_up, W, point_van_xy[1] + tol_up)
-    bottom_line = (0, H - tol_bottom, W, H - tol_bottom)
-
-    pad_left = 1 * W
-    pad_right = 1 * W
-    line1 = (-pad_left, H, point_van_xy[0], point_van_xy[1])
-    line2 = (W + pad_right, H, point_van_xy[0], point_van_xy[1])
-    p1 = tools_render_CV.line_intersection(upper_line, line1)
-    p2 = tools_render_CV.line_intersection(upper_line, line2)
-    p3 = tools_render_CV.line_intersection(bottom_line, line1)
-    p4 = tools_render_CV.line_intersection(bottom_line, line2)
-    src = numpy.array([(p1[0], p1[1]), (p2[0], p2[1]), (p3[0], p3[1]), (p4[0], p4[1])], dtype=numpy.float32)
-    dst = numpy.array([(0, 0), (target_W, 0), (0, target_H), (target_W, target_H)], dtype=numpy.float32)
-    #image = tools_draw_numpy.draw_convex_hull(image, [p1, p2, p3, p4], color=(36, 10, 48), transperency=0.5)
-    h_ipersp = cv2.getPerspectiveTransform(src, dst)
-
-    return h_ipersp
-# ----------------------------------------------------------------------------------------------------------------------
 def project_2D_BEV(X_source,H):
     res = cv2.perspectiveTransform(X_source.reshape(-1, 1, 2), H).reshape((-1, 2))
     return res
@@ -638,12 +615,13 @@ def draw_boxes(folder_out, folder_images, folder_labels_GT, folder_calib):
         image = tools_image.desaturate(cv2.imread(filename_image))
         H,W = image.shape[:2]
         target_BEV_W, target_BEV_H = int(H*0.75),H
+        point_van_xy = (631, 166)
 
 
         records = open(filename_label).readlines()
         colors = tools_draw_numpy.get_colors(len(records))
         image_2d = image.copy()
-        h_ipersp = get_h_ipersp(image,target_BEV_W, target_BEV_H)
+        h_ipersp = tools_render_CV.get_h_ipersp(image,target_BEV_W, target_BEV_H,point_van_xy)
         image_BEV = cv2.warpPerspective(image, h_ipersp, (target_BEV_W, target_BEV_H), borderValue=(32, 32, 32))
         for record,color in zip(records,colors):
             record = record.strip().split(' ')
